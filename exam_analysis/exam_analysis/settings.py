1aa0605d03665eb1275cc63baafe029553f128d6
Devendra Kumar Singh <devendra1997kumar@gmail.com>
2025-03-27 23:59:54 +0530

    sys.path.append(str(BASE_DIR))

--- exam_analysis/exam_analysis/settings.py
+++ exam_analysis/exam_analysis/settings.py
@@ -17,6 +17,7 @@ pymysql.install_as_MySQLdb()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+import my_exam
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
